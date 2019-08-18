04997b50fcf595701c2b2d30c1102c8bbbd7d326
unknown <e.fraffo@gmail.com>
2019-08-18 18:53:05 -0300

    18/08

--- js/homebanking.js
+++ js/homebanking.js
@@ -1,4 +1,7 @@
 //Declaración de variables
+var nombreUsuario = 'Esteban Raffo';
+var saldoCuenta = 100000;
+var limiteExtraccion = 3000;
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
@@ -10,15 +13,45 @@ window.onload = function() {
 
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-
+    var nuevoLimite = +prompt('Ingrese nuevo límite de extracción: ');
+    limiteExtraccion = nuevoLimite;
+    actualizarLimiteEnPantalla();
+    alert('El nuevo límite es: ' + limiteExtraccion);
 }
 
 function extraerDinero() {
+    var montoAExtraer = +prompt('Monto a Extraer');
+    var saldoAnterior = saldoCuenta;
 
+    if(montoAExtraer <= saldoCuenta){
+        if(montoAExtraer <= limiteExtraccion){
+            if(montoAExtraer % 100 == 0){
+                saldoCuenta -= montoAExtraer;
+                actualizarSaldoEnPantalla();
+                alert('Has retirado: ' + montoAExtraer + '\nSaldo anterior: ' + saldoAnterior + '\nSaldo Actual: ' + saldoCuenta);
+            }
+            else{
+                alert('Solo puedes extraer billetes de 100');
+            }
+        }
+        else{
+            alert('La cantidad de dinero que deseas extraer es mayor a tu límite de extracción');
+        }
+    }
+    else{
+        alert('No hay saldo disponible en tu cuenta para extraer esa cantidad de dinero');
+    }
+     
 }
 
-function depositarDinero() {
+// 2.3  Guía: parte 3 - Paso 2: Desarrollá la funcionalidad de pago de servicios
 
+function depositarDinero() {
+    var montoADepositar = +prompt('Monto a Depositar: ');
+    var saldoAnterior = saldoCuenta;
+    saldoCuenta += montoADepositar;
+    actualizarSaldoEnPantalla();
+    alert('Has depositado: ' + montoADepositar + '\nSaldo anterior: ' + saldoAnterior + '\nSaldo Actual: ' + saldoCuenta);
 }
 
 function pagarServicio() {
