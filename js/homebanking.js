15b34506a3b79301d04a20f8fbb79eafe846f734
unknown <e.fraffo@gmail.com>
2019-08-19 19:26:37 -0300

    19/08

--- js/homebanking.js
+++ js/homebanking.js
@@ -1,7 +1,15 @@
 //Declaración de variables
 var nombreUsuario = 'Esteban Raffo';
-var saldoCuenta = 100000;
+var saldoCuenta = 1000000;
 var limiteExtraccion = 3000;
+var precioAgua = 350;
+var precioTelefono = 425;
+var precioLuz = 210;
+var precioInternet = 570;
+var cuentaAmiga1 = 1234567;
+var cuentaAmiga2 = 7654321;
+var códigoSeguridad = 'abc123';
+
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
@@ -10,61 +18,164 @@ window.onload = function() {
     actualizarLimiteEnPantalla();
 }
 
+function esValorValido(valor){
+  
+    if(valor != 0) 
+        return true; 
+    else
+        return false;
+}
 
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
     var nuevoLimite = +prompt('Ingrese nuevo límite de extracción: ');
-    limiteExtraccion = nuevoLimite;
-    actualizarLimiteEnPantalla();
-    alert('El nuevo límite es: ' + limiteExtraccion);
+    
+    if(esValorValido(nuevoLimite)){
+        limiteExtraccion = nuevoLimite;
+        actualizarLimiteEnPantalla();
+        alert('El nuevo límite es: ' + limiteExtraccion);
+    }
+    else{
+        return;
+    }
 }
 
 function extraerDinero() {
-    var montoAExtraer = +prompt('Monto a Extraer');
     var saldoAnterior = saldoCuenta;
-
-    if(montoAExtraer <= saldoCuenta){
-        if(montoAExtraer <= limiteExtraccion){
-            if(montoAExtraer % 100 == 0){
-                saldoCuenta -= montoAExtraer;
-                actualizarSaldoEnPantalla();
-                alert('Has retirado: ' + montoAExtraer + '\nSaldo anterior: ' + saldoAnterior + '\nSaldo Actual: ' + saldoCuenta);
+    var montoAExtraer = +prompt('Monto a Extraer');
+    
+    if(esValorValido(montoAExtraer)){
+        if(montoAExtraer <= saldoCuenta){
+            if(montoAExtraer <= limiteExtraccion){
+                if(montoAExtraer % 100 == 0){
+                    saldoCuenta -= montoAExtraer;
+                    actualizarSaldoEnPantalla();
+                    alert('Has retirado: ' + montoAExtraer + '\nSaldo anterior: ' + saldoAnterior + '\nSaldo Actual: ' + saldoCuenta);
+                }
+                else{
+                    alert('Solo puedes extraer billetes de 100');
+                }
             }
             else{
-                alert('Solo puedes extraer billetes de 100');
+                alert('La cantidad de dinero que deseas extraer es mayor a tu límite de extracción');
             }
         }
         else{
-            alert('La cantidad de dinero que deseas extraer es mayor a tu límite de extracción');
+            alert('No hay saldo disponible en tu cuenta para extraer esa cantidad de dinero');
         }
     }
     else{
-        alert('No hay saldo disponible en tu cuenta para extraer esa cantidad de dinero');
+        return;
     }
-     
 }
 
-// 2.3  Guía: parte 3 - Paso 2: Desarrollá la funcionalidad de pago de servicios
 
 function depositarDinero() {
-    var montoADepositar = +prompt('Monto a Depositar: ');
     var saldoAnterior = saldoCuenta;
-    saldoCuenta += montoADepositar;
-    actualizarSaldoEnPantalla();
-    alert('Has depositado: ' + montoADepositar + '\nSaldo anterior: ' + saldoAnterior + '\nSaldo Actual: ' + saldoCuenta);
+    var montoADepositar = +prompt('Monto a Depositar: ');
+
+    if(esValorValido(montoADepositar)){
+        saldoCuenta += montoADepositar;
+        actualizarSaldoEnPantalla();
+        alert('Has depositado: ' + montoADepositar + '\nSaldo anterior: ' + saldoAnterior + '\nSaldo Actual: ' + saldoCuenta);
+    }    
+    else{
+        return;
+    }
 }
 
 function pagarServicio() {
+    var opcion = +prompt('Ingrese el número que corresponda con el servicio que querés pagar' + '\n1- Agua' + '\n2- Luz' + '\n3- Internet' 
+    + '\n4- Teléfono');
 
+    switch(opcion){
+        case 1:
+            pagarEsteServicio(1, precioAgua);
+            break;
+        case 2:
+            pagarEsteServicio(2, precioLuz);
+            break;
+        case 3:
+            pagarEsteServicio(3, precioInternet);
+            break;
+        case 4:
+            pagarEsteServicio(4, precioTelefono);
+            break;
+        default:
+            alert('No existe el servicio seleccionado');
+            break;
+    }
+}
+
+function pagarEsteServicio(idServicio, precioServicio){
+    var servicios = ['','Agua', 'Luz', 'Internet', 'Teléfono'];
+
+    if(precioServicio <= saldoCuenta){
+        var saldoAnterior = saldoCuenta; 
+        saldoCuenta -= precioServicio;
+        alert('Has pagado el servicio' + servicios[idServicio] + '\nSaldo anterior: ' + saldoAnterior + '\nDinero descontado: ' + 
+        precioServicio + '\nSaldo Actual: ' + saldoCuenta);
+        actualizarSaldoEnPantalla();
+    }
+    else{
+        alert('No hay suficiente saldo en tu cuenta para pagar este servicio');
+    }
 }
 
 function transferirDinero() {
+    var monto = +prompt('Ingrese monto a transferir');
 
+    if(esValorValido(monto)){
+        if(monto <= saldoCuenta){
+            let cuenta = +prompt('Ingrese la cuenta a donde va a transferir');
+            if(esCtaAmiga(cuenta)){
+                saldoCuenta -= monto;
+                actualizarSaldoEnPantalla();
+                alert('Se han transferido: ' + monto + '\nCuenta destino: ' + cuenta);
+            }
+            else{
+                alert('Solo puede transferirse dinero a una cuenta amiga');
+            }
+        }
+        else{
+            alert('No tiene saldo suficiente para la transferencia solicitada');
+        }
+    }
+    else{
+        return;
+    }
+
+}
+
+function esCtaAmiga(cuenta){
+    if(cuenta == cuentaAmiga1 || cuenta == cuentaAmiga2){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 function iniciarSesion() {
+    var clave = prompt('Ingrese su clave de cuenta');
 
+    if(clave == códigoSeguridad){
+        alert('Bienvenido ' + nombreUsuario + ' ya puedes comenzar a realizar operaciones');
+        cargarNombreEnPantalla();
+        actualizarSaldoEnPantalla();
+        actualizarLimiteEnPantalla();
+    }
+    else{
+        saldoCuenta = 0;
+        // document.getElementsByClassName("white-container").style.display = 'none';
+        // document.getElementsByTagName('button').style.display = 'none';
+        alert('Código incorrecto. Tu dinero ha sido retenido por cuestiones de seguridad');
+    }
 }
+
+// function ocultar(){
+//     document.getElementsByClassName("white-container").style.visibility = 'hidden';
+// }
 
 //Funciones que actualizan el valor de las variables en el HTML
 function cargarNombreEnPantalla() {
